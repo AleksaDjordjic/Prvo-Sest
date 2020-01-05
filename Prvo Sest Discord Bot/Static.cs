ba96befdaa005141fdd0d0c26334052207910db6
AleksaDjordjic <33227154+AleksaDjrodjic@users.noreply.github.com>
2020-01-05 01:27:49 +0100

    Debug Settings and Language Version Fixes

--- Prvo Sest Discord Bot/Static.cs
+++ Prvo Sest Discord Bot/Static.cs
@@ -3,5 +3,9 @@
 public static class Static
 {
     public static readonly Color Color = new Color(255, 0, 0);
+#if DEBUG
+    public static readonly string Prefix = "-";
+#else
     public static readonly string Prefix = "!";
+#endif
 }
